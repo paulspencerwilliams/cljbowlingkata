5bba1e9c8fbee81e8b3fbce720393e1512de7043
Paul Williams <paul@paulswilliams.me.uk>
2017-01-14 15:18:07 +0000

    12 strikes = 300

--- src/bowlingkata/core.clj
+++ src/bowlingkata/core.clj
@@ -1,13 +1,21 @@
 (ns bowlingkata.core)
 
 (defn spare? [first second] (= 10 (+ first second)))
+(defn strike? [first] (= "X" first))
+
+(defn score-type [first second]
+  (cond
+    (strike? first) :strike
+    (spare? first second) :spare
+    :else :normal))
 
 (defn add-next-if-spare
   [turn-with-next]
-  (let [[first-roll second-roll next-roll] turn-with-next]
-    (if (spare? first-roll second-roll)
-      (+ first-roll second-roll next-roll)
-      (+ first-roll second-roll))))
+  (let [[first-roll second-roll next-roll final-roll] turn-with-next]
+    (case (score-type first-roll second-roll)
+      :strike (+ 10 10 10)
+      :spare (+ first-roll second-roll next-roll)
+      :normal (+ first-roll second-roll))))
 
 (defn score
   "Calculate score for a series of rolls"
@@ -15,4 +23,4 @@
   (reduce
     +
     (map add-next-if-spare
-         (take 10 (partition 3 2 [0] rolls)))))
+         (take 10 (partition 4 2 [0] rolls)))))
--- test/bowlingkata/core_test.clj
+++ test/bowlingkata/core_test.clj
@@ -9,3 +9,7 @@
 (deftest eleven-fives
   (testing "Ten spares (5/5) and a final 5 = 150"
     (is (= (score [5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5]) 150))))
+
+(deftest twelve-strikes
+  (testing "12 strikes = 300"
+    (is (= (score ["X" 0 "X" 0 "X" 0 "X" 0 "X" 0 "X" 0 "X" 0 "X" 0 "X" 0 "X" 0 "X" "X"])))))
